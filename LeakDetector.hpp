48889e6199de6371da222d36ed6f44775036954f
wujiu913 <924492369@qq.com>
2016-10-23 22:52:42 -0500

    Add files via upload

---
+++ LeakDetector.hpp
@@ -0,0 +1,35 @@
+//
+//  LeakDetector.hpp
+//  LeakDetector
+//
+//  Created by changkun on 16/9/13.
+//  Copyright © 2016 shiyanlou.com, all rights reserved.
+//
+#ifndef __LEAK_DETECTOR__
+#define __LEAK_DETECTOR__
+
+void* operator new(size_t _size, char *_file, unsigned int _line);
+void* operator new[](size_t _size, char *_file, unsigned int _line);
+// 此处宏的作用在 LeakDetector.cpp 中说明
+#ifndef __NEW_OVERLOAD_IMPLEMENTATION__
+#define new	new(__FILE__, __LINE__)
+#endif
+
+class _leak_detector
+{
+public:
+    static unsigned int callCount;
+    _leak_detector() noexcept {
+        ++callCount;
+    }
+    ~_leak_detector() noexcept {
+        if (--callCount == 0)
+            LeakDetector();//什么意思？
+    }
+private:
+    static unsigned int LeakDetector() noexcept;
+};
+static _leak_detector _exit_counter;//这个对象的目的
+
+#endif
+
